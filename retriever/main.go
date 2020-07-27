2a09b548cab5669c539f072415d005c1cd0ae8de
imdszxs <760140273@qq.com>
2020-07-27 23:47:39 +0800

    学习代码

--- retriever/main.go
+++ retriever/main.go
@@ -7,15 +7,53 @@ import (
 	"time"
 )
 
+const url = "https://www.imooc.com"
+
 type Retriever interface {
 	Get(url string) string
 }
 
-func download(r Retriever) string {
-	return r.Get("https://www.imooc.com")
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
 }
+
+func download(r Retriever) string {
+	return r.Get(url)
+}
+
+func post(p Poster)  {
+	p.Post(url,
+		map[string]string{
+			"name":"imdszxs",
+			"course":"golang",
+		})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func session(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another faked imooc.com",
+	})
+	return s.Get(url)
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	switch v := r.(type) {
+	case *mooc.Retriever:
+		fmt.Println("contents:", v.Contents)
+	case real.Retriever:
+		fmt.Println("userAgent:", v.UserAgent)
+	}
+}
+
 func main() {
-	var r Retriever = mooc.Retriever{"this is fake imooc.com"}
+	var r Retriever = &mooc.Retriever{"this is fake imooc.com"}
 	inspect(r)
 
 	r = real.Retriever{
@@ -25,20 +63,15 @@ func main() {
 	inspect(r)
 
 	// type assertion
-	if moocRetriever, ok := r.(mooc.Retriever); ok {
+	if moocRetriever, ok := r.(*mooc.Retriever); ok {
 		fmt.Println(moocRetriever.Contents)
 	} else {
 		fmt.Println("not a mooc retriever")
 	}
-	//fmt.Println(download(r))
+
+	fmt.Println("try a session")
+	s := mooc.Retriever{"this is fake imooc.com"}
+	fmt.Println(session(&s))
 }
 
-func inspect(r Retriever) {
-	fmt.Printf("%T %v\n", r, r)
-	switch v := r.(type) {
-	case mooc.Retriever:
-		fmt.Println("contents:", v.Contents)
-	case real.Retriever:
-		fmt.Println("userAgent:", v.UserAgent)
-	}
-}
+
