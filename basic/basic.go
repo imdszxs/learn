d60add51f15e8408773e8495fd4c35069566bc94
zhangxiang <xiang.zhang@ifchange.com>
2020-07-28 15:22:07 +0800

    basic

---
+++ basic/basic.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	aa = 3
+	ss = "kkk"
+	bb = true
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d, %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction()  {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter()  {
+	a, b, c, s := 3, 4, true, "def"
+	b = 5
+	fmt.Println(a, b, c, s)
+}
+
+func euler() {
+	fmt.Printf("%.3f %.3f\n",
+		cmplx.Exp(1i * math.Pi) + 1,
+		cmplx.Pow(math.E, 1i * math.Pi) + 1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(aa, ss, bb)
+
+	euler()
+	triangle()
+}
