bedc337f819bb5649e02c6474218b7bebecba145
imdszxs <760140273@qq.com>
2020-08-16 23:19:05 +0800

    单元测试

--- basic/basic/basic.go
+++ basic/basic/basic.go
@@ -43,9 +43,13 @@ func euler() {
 
 func triangle() {
 	var a, b int = 3, 4
+	fmt.Println(calcTriangle(a, b))
+}
+
+func calcTriangle(a, b int) int {
 	var c int
 	c = int(math.Sqrt(float64(a*a + b*b)))
-	fmt.Println(c)
+	return c
 }
 
 func consts()  {
