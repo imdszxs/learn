999f025ff642d0d001b8fa95cd208c4b40d3b0c3
imdszxs <760140273@qq.com>
2020-08-17 22:57:09 +0800

    测试

--- container/nonrepeatingsubstr/nonrepeating.go
+++ container/nonrepeatingsubstr/nonrepeating.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 )
 
+var lastOccurred = make([]int, 0xffff)
+
 func lengthOfNonRepeatingSubStr(s string) int {
-	lastOccurred := make(map[rune]int)
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
 	start := 0
 	maxLength := 0
 
 	for i, ch := range []rune(s) {
-		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
 			start = lastI + 1
 		}
 		if i-start+1 > maxLength {
--- container/nonrepeatingsubstr/nonrepeating_test.go
+++ container/nonrepeatingsubstr/nonrepeating_test.go
@@ -35,7 +35,14 @@ func TestSubstr(t *testing.T)  {
 
 func BenchmarkSubstr(b *testing.B) {
 	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s = s + s
+	}
 	ans := 8
+
+	b.Logf("len(s) = %d", len(s))
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		actual := lengthOfNonRepeatingSubStr(s)
 		if actual != ans {
