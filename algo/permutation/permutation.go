619fc4b0a983e062a0f224b15f4b58f01e1e0e59
zhangxiang <xiang.zhang@ifchange.com>
2020-08-27 17:40:05 +0800

    algo

---
+++ algo/permutation/permutation.go
@@ -0,0 +1,45 @@
+package strlen
+
+var lastOccurred = make([]int, 0xffff)
+
+func lengthOfNonRepeatingSubStr(s string) int {
+	for i := range lastOccurred {
+		lastOccurred[i] = -1
+	}
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []rune(s) {
+		if lastI := lastOccurred[ch]; lastI != -1 && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+
+	return maxLength
+}
+
+//func main() {
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("abcabcbb"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("bbbbb"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("pwwkew"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr(""))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("b"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("abcdef"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("这里是慕课网"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr("一二三二一"))
+//	fmt.Println(
+//		lengthOfNonRepeatingSubStr(
+//			"黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"))
+//}
